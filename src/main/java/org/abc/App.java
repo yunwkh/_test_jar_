b4f9e1742345cd520f9023ca78aea723f56324ef
Yun Wei, Khong <khong.yun.wei@f-secure.com>
2016-09-28 15:55:54 +0800

    import java.sql and add build manifest in pom

--- src/main/java/org/abc/App.java
+++ src/main/java/org/abc/App.java
@@ -1,5 +1,21 @@
 package org.abc;
 
+import com.mysql.jdbc.SQLError;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -9,5 +25,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        try {
+            Connection dbConn = DriverManager.getConnection("jdbc:mysql://localhost/test", "user", "passwd");
+            System.out.println("db connection: OK");
+        }
+        catch (SQLException e) {
+            System.out.println("db connection: Failed");
+        }
     }
 }
